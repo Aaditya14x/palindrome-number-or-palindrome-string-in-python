1701e6d1380229d2d7b0fbe6071554870299cdcf
Aaditya dubey <115134350+Aaditya14x@users.noreply.github.com>
2023-03-11 22:17:12 +0530

    Add files via upload
    This Programs will tell you that the number or string is palindrome or not

---
+++ Palindrome number in python.py
@@ -0,0 +1,5 @@
+i=(input("enter any number :"))
+if(i==i[ ::-1]):
+  print("Palindrome number")
+else:
+    print("Non Palindrome number")
---
+++ Palindrome string in python.py
@@ -0,0 +1,5 @@
+i=(input("enter any string :"))
+if(i==i[ ::-1]):
+  print("Palindrome number")
+else:
+    print("Non Palindrome number")
